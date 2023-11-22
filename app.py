ffbb0baf7153afc0793068ffd2a93111ea964546
GSHcodes <125884570+GSHcodes@users.noreply.github.com>
2023-11-22 22:30:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-from flask import Flask
-import os
+from flask import Flask, request
+import json
+
+from forex_python.converter import CurrencyRates
+currency=CurrencyRates()
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, world!'
+@app.route('/', methods=['POST'])
+def index():
+    if request.method == 'POST':
+        data = request.get_json()
+        source_currency = data['queryResult']['parameters']['unit-currency']['currency']
+        amount_source = data['queryResult']['parameters']['unit-currency']['amount']
+        to_currency = data['queryResult']['parameters']['currency-name']
+        converted = currency.convert(source_currency, to_currency, amount_source)
+        response = {
+            'fulfillmentText': "{}  {}  is equivalent to  {}  {}".format(amount_source,source_currency,round(converted,2),to_currency),  # You can modify this message accordingly
+            # Additional fields if needed
+        }
+
+        # Returning the response as a JSON string
+        return json.dumps(response)
+    else:
+        return "Method Not Allowed", 405
+
+if __name__ == '__main__':
+    print("Server starting...")
+    app.run(debug=True)
