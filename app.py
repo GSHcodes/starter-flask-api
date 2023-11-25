55ced0ca52d0beb1a9e75e9ba3f95d472a3736bb
GSHcodes <125884570+GSHcodes@users.noreply.github.com>
2023-11-25 10:12:52 +0530

    Update app.py
    added age calculation functionality

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-from flask import Flask, request
-import json
-
-from forex_python.converter import CurrencyRates
-currency=CurrencyRates()
+from flask import Flask, request, jsonify
+from calculations import calculate_age , convert_currency
 
 app = Flask(__name__)
 
@@ -10,18 +7,39 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         data = request.get_json()
-        source_currency = data['queryResult']['parameters']['unit-currency']['currency']
-        amount_source = data['queryResult']['parameters']['unit-currency']['amount']
-        to_currency = data['queryResult']['parameters']['currency-name']
-        converted = currency.convert(source_currency, to_currency, amount_source)
-        print(str(amount_source) + " " + source_currency + " is equivalent to " + str(converted) + " " + to_currency)
-        response = {
-            'fulfillmentText': "{}  {}  is equivalent to  {}  {}".format(amount_source,source_currency,round(converted,2),to_currency),  # You can modify this message accordingly
-            # Additional fields if needed
-        }
+        intent_display_name = data['queryResult']['intent']['displayName']
 
-        # Returning the response as a JSON string
-        return json.dumps(response)
+        if intent_display_name == 'CurrencyConvert':
+            #fetch data from json
+            source_currency = data['queryResult']['parameters']['unit-currency']['currency']
+            amount_source = data['queryResult']['parameters']['unit-currency']['amount']
+            to_currency = data['queryResult']['parameters']['currency-name']
+            #convert usign forex python
+            converted = convert_currency(source_currency, amount_source, to_currency)
+            #response to dialogflow
+            response = {
+                'fulfillmentText': "{} {} is equivalent to {} {}".format(amount_source, source_currency, converted, to_currency)
+            }
+
+        elif intent_display_name == 'ageCalculator':
+            #fetch data
+            datetime = data['queryResult']['parameters']["date-time"][0]
+            dob_str = datetime.split("T")[0]
+            #calculate
+            age_result = calculate_age(dob_str)
+            print(age_result)
+            #response to dialogflow
+            response = {
+                'fulfillmentText': "{}".format(age_result)
+            }
+
+        else:
+            # Default response if intent is not recognized
+            response = {
+                'fulfillmentText': "Intent not recognized"
+            }
+
+        return jsonify(response)
     else:
         return "Method Not Allowed", 405
 
